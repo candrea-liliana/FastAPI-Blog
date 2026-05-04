4fe9c50d3f46c956503b6ec65bb580f2680d1900
Liliana Candrea <candrealiliana94@gmail.com>
2026-05-04 17:00:13 +0300

    Add some client-side forms using Vanilla JavaScript to connect with the FastAPI backend. Create, edit and delete from the browser, along with using Bootstrap modals for user feedback and organinzig API responses.

--- routers/users.py
+++ routers/users.py
@@ -74,7 +74,8 @@ async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_d
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
-        .where(models.Post.id == user_id),
+        .where(models.Post.id == user_id)
+        .order_by(models.Post.date_posted.desc()),
     )
     posts = result.scalars().all()
     return posts
